cc0ef20470db5355fa13f5d80e576f8d9d3384c5
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-15 12:05:28 +0100

    Add Bill Splitter

---
+++ billSplitter.js
@@ -0,0 +1,10 @@
+let totalBill = 15000;
+let numberOfPeople = 4;
+let tipPercent = 10;
+let tipAmount = totalBill / tipPercent
+let tBillTip = totalBill + tipAmount
+let billHead = tBillTip / numberOfPeople
+
+console.log(tipAmount)
+console.log(tBillTip)
+console.log(billHead)
