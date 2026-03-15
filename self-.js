88049fc13b03fe844171aad357669e1b9198a949
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-15 11:41:38 +0100

    Add Self Introduction

---
+++ self-.js
@@ -0,0 +1,7 @@
+let firstName = "Emmanuel"
+let lastName = "Nonyerem"
+let age = 20
+let country = "Nigeria"
+let hobby = "Coding"
+
+console.log(`My fullname is ${lastName} ${firstName}. I am ${age} years old and i live in ${country}. In my free time, I enjoy ${hobby}`)
