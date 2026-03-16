1c9b58d53a866fa524b169df6be4fba7f4e6be73
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-16 07:46:37 +0100

    Add Number Range

---
+++ numReg.js
@@ -0,0 +1,11 @@
+let num = 55
+  if (num >= 1 && num <= 33) {
+    console.log("Low");
+  } else if (num >= 34 && num <= 66) {
+    console.log("Medium");
+  } else if (num >= 67 && num <= 100) {
+    console.log("High");
+  } else {
+    console.log("Out of range");
+  }
+
